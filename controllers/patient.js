d5c314dc742ea411f1a2030cb3b37939b661e2b2
RUYANGA <ruyangam15@gmail.com>
2025-04-06 17:54:54 +0200

    new

--- controllers/patient.js
+++ controllers/patient.js
@@ -285,6 +285,113 @@ const Login=async(req,res,next)=>{
   
 };
 
+const forgetPassword=async(req,res,next)=>{
+  try {
+        const {Email}=req.body;
+
+      const errors=validationResult(req);
+      if(!errors.isEmpty()){
+          const errorFormat=errors.array().map(err=>({
+              message:err.msg
+          }))
+          return res.status(400).json({error:errorFormat});
+      };
+
+      const token= crypto.randomBytes(32).toString('hex');
+
+      const patient=await Patient.findOne({Email:Email})
+
+      transipoter.sendMail({
+
+        from:process.env.EMAIL,
+        to:Email,
+        subject:'OTP VERIFICATION CODE',
+        html:` <!DOCTYPE html>
+        <html>
+        <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Welcome Email</title>
+        <link href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+        </head>
+        <body style="margin: 0; padding: 0; font-family: Arial, sans-serif; background-color: #f4f4f4;">
+        <table role="presentation" style="width: 100%; border-collapse: collapse;">
+            <tr>
+            <td style="padding: 0;">
+                <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff;">
+                <!-- Header -->
+                <div style="background-color: #4A90E2; padding: 24px; text-align: center;">
+                    <h1 style="color: #ffffff; margin: 0; font-size: 26px;">Welcome to BINARY HUB  Intern</h1>
+                </div>
+                
+                <!-- Content -->
+                <div style="padding: 24px; line-height: 1.6;">
+                    <p style="margin-top: 0; color: #333333; font-size: 16px;">Hello 👋 ${user.Lname},</p>
+                    
+                    <p style="color: #333333; font-size: 16px;">Thank you for signing up! We're excited to have you on board.</p>
+                    
+                    <p style="color: #333333; font-size: 16px;">Here are verification code  to help you get started this code expired in 15 minutes:</p>
+                    
+                
+                    
+                    <div style="margin: 30px 0; text-align: center;">
+                    <h1 style="background-color: #4A90E2; color: #ffffff; text-decoration: none; padding: 12px 24px; border-radius: 4px; font-weight: bold; display: inline-block;"><a href='http://localhost:2000/reset-password/${token}'>Reset password</a></h1>
+                    </div>
+                    
+                    <p style="color: #333333; font-size: 16px;">If you have any questions, just reply to this email. We're always here to help!</p>
+                    
+                    <p style="color: #333333; font-size: 16px;">Best regards,<br>Merci RUYANGA</p>
+                </div>
+                
+                <!-- Footer -->
+                <div style="background-color: #f4f4f4; padding: 24px; text-align: center;">
+                    <p style="margin: 0; color: #777777; font-size: 14px;">
+                    © 2025 Your Company. All rights reserved.
+                    </p>
+                    <p style="margin: 10px 0 0; color: #777777; font-size: 14px;">
+                    <a href="#" style="color: #777777; text-decoration: underline;">Unsubscribe</a> |
+                    <a href="#" style="color: #777777; text-decoration: underline;">Privacy Policy</a>
+                    </p>
+                    <div style="margin-top: 20px;">
+                    <a href="https://www.facebook.com/ruyanga.merci.1" style="display: inline-block; margin: 0 10px; color: #4A90E2;">
+                    
+                        <i class="bi bi-facebook"></i>
+                    </a>
+                    <a href="https://x.com/RuyangaM" style="display: inline-block; margin: 0 10px; color: #4A90E2;">
+                        <i class="bi bi-twitter-x"></i>
+                    </a>
+                    <a href="https://github.com/RUYANGA" style="display: inline-block; margin: 0 10px; color: #4A90E2;">
+                        <i class="bi bi-github"></i>
+                    </a>
+                    </div>
+                </div>
+                </div>
+            </td>
+            </tr>
+        </table>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        </body>
+        </html>`
+
+      })
+
+      const currentDate=new Date();
+      const tokenExpired=addMinutes(currentDate,30);
+
+      patient.token=token;
+      patient.tokenExpired=tokenExpired;
+      await patient.save()
+
+      res.status(200).json({message:`Rest password link sent to ${Email}`})
+
+    } catch (error) {
+      const errors= new Error(error);
+      errors.statusCode=500;
+      return next(errors);
+  }
+
+}
+
 
 const lognOut=async(req,res,next)=>{
   try {
