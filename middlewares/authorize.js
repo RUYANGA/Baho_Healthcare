633bab21e6f71b4b570d6e3d41980b25f5de0564
RUYANGA <ruyangam15@gmail.com>
2025-04-11 18:52:11 +0200

    new

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -1,9 +1,12 @@
 
+//middleware to validate user login session
 const unauthrized=async(req,res,next)=>{
     if(!req.session.user)return res.status(400).json({message:"Unauthorized. Please log in first."});
         return next()
 }
 
+
+//middleware to allow admin 
 const Admin = (req, res, next) => {
     if (!req.session.user.isAdmin) {
         return res.status(403).json({ message: "Access denied. You are not an admin." });
