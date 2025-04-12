7fa380ed982f7812f9de1ad94bc26378648c17be
RUYANGA <ruyangam15@gmail.com>
2025-04-12 08:23:34 +0200

    new

---
+++ controllers/Admin.js
@@ -0,0 +1,4 @@
+const Patient=require('../modeles/Patient');
+const Doctor=require('../modeles/Doctor');
+const Apointment=require('../modeles/Apointment');
+const {validationResult}=require('express-validator');
